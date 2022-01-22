733745ea4e13746fde9baa957aa8d25960533497
hasan-py <hasan29351@gmail.com>
2022-01-22 13:55:09 +0600

    Binary Search Implement

--- searchingAlgorithom/binarySearch.py
+++ searchingAlgorithom/binarySearch.py
@@ -1,23 +1,33 @@
-import math
+# def binarySearch(arr, searchValue):
+#     left = 0
+#     right = len(arr) - 1
+#     while left <= right:
+#         mid = (left + right) // 2
+#         if arr[mid] == searchValue:
+#             return mid
+#         elif arr[mid] > searchValue:
+#             right = mid - 1
+#         else:
+#             left = mid + 1
+#     return -1
+
+# print(binarySearch([1, 2, 3, 4, 5, 6, 7, 10], 2))  # 7
 
 
-def binarySearch(arr, searchValue):
-    left = 0
-    right = len(arr) - 1
-
-    while left < right:
-        mid = math.floor(left + right / 2)
-
+def binarySearchWithRecursion(arr, left, right, searchValue):
+    if left <= right:
+        mid = (left + right) // 2
         if arr[mid] == searchValue:
             return mid
 
         if arr[mid] > searchValue:
-            right = mid - 1
+            return binarySearchWithRecursion(arr, left, mid - 1, searchValue)
 
         elif arr[mid] < searchValue:
-            left = mid + 1
+            return binarySearchWithRecursion(arr, mid + 1, right, searchValue)
 
     return -1
 
 
-print(binarySearch([1, 2, 3, 5, 6, 8, 10, 12], 12))
+arr = [1, 1, 2, 3, 5, 6, 8, 10, 12]
+print(binarySearchWithRecursion(arr, 0, len(arr) - 1, 5))
